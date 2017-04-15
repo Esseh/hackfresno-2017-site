46f70734bee54e2a3820107de3c17cdf32500169
Esseh <kennethwilleford@gmail.com>
2017-04-15 08:57:44 -0700

    App Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,6 @@ var LastModified = 0;
 var PendingAction = "MyGroups"; // The Action to be Performed
 var SidebarMode = "";	        // MyGroups | SearchGroups
 var ActionData = "";            // contains Metadata to be processed by controller
-var ID_OF_EVENT = 0;
 var ID_OF_DISCUSSION = 0;
 var ID_OF_GROUP = 0;
 
@@ -15,10 +14,8 @@ setInterval(function View(){
 			if(LastModified != TimeDelta){
 				console.log(`ViewMode = `,ViewMode);
 				LastModified = TimeDelta;
-				if(ViewMode == "about"){ updateDiscussion(); } 
 				if(ViewMode == "about"){ updateAbout(); } 
 				if(ViewMode == "events"){updateEvents();} 
-				if(ViewMode == "event"){ updateEventInfo(); } 
 				if(ViewMode == "threads"){ updateThreads(); } 
 				if(ViewMode == "thread"){ updatePosts(); } 
 				if(ViewMode == "discussion"){ updateDiscussion(); } 
@@ -56,7 +53,7 @@ function makePost(){
 function makeDiscussionPost(){
 	var action = ActionData;
 	ActionData = "";
-	$.post('/api/controller', { groupID: ID_OF_GROUP, control: "makeDiscussionPost", post: action });		
+	$.post('/api/controller', { groupID: ID_OF_GROUP, control: "makeDiscussionPost", post: action, discussionID: ID_OF_GROUP });		
 }
 
 // Occurs After Making New Thread in Threads
@@ -70,6 +67,7 @@ function makeThread(){
 function makeEvent(){
 	// ActionData is comma delimited
 	var val = ActionData.split(",");
+	console.log(val)
 	ActionData = "";
 	$.post('/api/controller', { groupID: ID_OF_GROUP, control: "makeEvent", title: val[0], what: val[1], when: val[2], where: val[3] });
 }
@@ -90,7 +88,7 @@ function findGroups(){
 	var val = ActionData.split(",");
 	ActionData = "";
 	$.post('/api/findgroups', { university: val[0], field: val[1], subject: val[2]},function(data){
-		var arr = JSON.parse(data);
+		var arr = JSON.parse(escape(data));
 		var responseHTML = "";
 		for(var i = 0; i < arr.length; i++){
 			responseHTML += '<li onclick="addGroupTrigger(event)" id="id'+arr[i].id+'"><a href="#">'+arr[i].title+'</a></li>';
@@ -122,7 +120,7 @@ function loadGroup(){
 // Occurs after start of program and after addGroup/createGroup
 function loadMyGroups(){
 	$.post('/api/mygroups',{},function(data){
-		var arr = JSON.parse(data);
+		var arr = JSON.parse(escape(data));
 		var responseHTML = "";
 		for(var i = 0; i < arr.length; i++){
 			responseHTML += '<li onclick="loadGroupTrigger(event)" id="id'+arr[i].id+'"><a href="#">'+arr[i].title+'</a></li>';
@@ -134,23 +132,96 @@ function loadMyGroups(){
 
 // VIEW Functions
 function updateDiscussion(){
-	
+	$.post("/api/view",{groupID:ID_OF_GROUP,control:"discussionPosts"},function(data){
+		console.log(data);
+		var arr = JSON.parse(escape(data));
+		var responseHTML = "";
+		responseHTML += '<li>'
+		responseHTML += '<span class="username">Bot: </span>'
+		responseHTML += '<span class="usermessage">Welcome to Open Discussion!</span>'
+		responseHTML += '</li>'
+		var currUser = "";
+		for(var i = 0; i < arr.length; i++){
+			if(arr[i].poster != currUser){
+				if(i != 0){ responseHTML += '</li>'; }
+				responseHTML += '<li>'
+				responseHTML += '<span class="username">'+arr[i].poster+'</span>';
+				currUser = arr[i].poster;
+			}
+			responseHTML += '<span class="usermessage">'+arr[i].post+'</span>'
+		}
+		if(arr.length != 0){ responseHTML+='</li>';}
+		document.getElementById("postList").innerHTML = responseHTML;
+		window.scrollTo(0,document.body.scrollHeight);
+	});
 }
 function updateAbout(){
-	
+	$.post("/api/view",{groupID:ID_OF_GROUP,control:"about"},function(data){
+		var d = JSON.parse(data);
+		document.getElementById("postList").innerHTML = '<li><span class="username">'+d.about+'</span></li>';
+	});
 }
 function updateEvents(){
-	
+	var arr = [1];
+	var waitLength = 0;
+	$.post("/api/view",{groupID:ID_OF_GROUP,control:"events"},function(data){
+		arr = JSON.parse(data).reverse();
+	}).done(function(){
+		$.each(arr,function(i,v){
+			$.post("/api/view",{goupID:ID_OF_GROUP,control:"eventInfo",eventID:v.id},function(data){
+				arr[i].data = JSON.parse(data);
+			}).done(function(){waitLength++;});
+		});
+	});
+	var timer = setInterval(function(){
+		console.log("tick")
+		if(waitLength == arr.length){
+			while(waitLength != arr.length){}
+			var responseHTML = "";
+				responseHTML += "<li>"
+				responseHTML += '<span class="username">Bot: </span>'
+				responseHTML += '<span class="usermessage">Why not check out one of the cool events posted here?...</span>'
+				responseHTML += "</li>"
+			for(var i = 0; i < arr.length; i++){
+				responseHTML += "<li>"
+				responseHTML += '<span class="username">'+arr[i].title+'</span>'
+				responseHTML += '<span class="usermessage">'+arr[i].data.what+'</span>'
+				responseHTML += '<span class="usermessage">'+arr[i].data.when+'</span>'
+				responseHTML += '<span class="usermessage">'+arr[i].data.where+'</span>'
+				responseHTML += "</li>"
+			}
+			document.getElementById("postList").innerHTML = responseHTML;
+			window.scrollTo(0,document.body.scrollHeight);	
+			clearInterval(timer);
+		}
+	},100);
 }
-function updateEventInfo(){
-	
-}
+
 function updateThreads(){
-	
+	$.post("/api/view",{groupID:ID_OF_GROUP, control:"threadTitles"},function(data){
+		console.log(data);
+		var arr = JSON.parse(data);
+		var responseHTML = "";
+		responseHTML += "<li>"
+		responseHTML += '<span class="username">Bot: </span>'
+		responseHTML += '<span class="usermessage">Click on a thread title to open it!</span>'
+		responseHTML += "</li>"
+		for(var i = 0; i < arr.length; i++){
+			responseHTML += '<a style="z-index:999999;" href="#">';
+			responseHTML += '<li onclick="openThreadTrigger(event)" id="id'+arr[i].id+'">';
+			responseHTML += '<span class="username">'+arr[i].title+'</span>';
+			responseHTML += '</li>';
+			responseHTML += '</a>';
+		}
+		document.getElementById("postList").innerHTML = responseHTML;
+		window.scrollTo(0,document.body.scrollHeight);	
+	})
 }
+
 function updatePosts(){
 	
 }
+
 function newGroupPrompt(){
 	ActionData = prompt("What will your group be named?");
 	ActionData += "," + prompt("Which univeristy?");
@@ -162,9 +233,35 @@ function newGroupPrompt(){
 
 function searchTrigger(e){
 	if(e.keyCode == 13){
-		ActionData = document.getElementById("sideBarSearch").value
+		ActionData = document.getElementById("sideBarSearch").value;
 		document.getElementById("sideBarSearch").value = "";
 		PendingAction = "FindGroups";	
+	}
+}
+
+function submitTrigger(e){
+	if(e.keyCode == 13){
+		if(ViewMode == "discussion"){
+			ActionData = document.getElementById("submissionArea").value;
+			document.getElementById("submissionArea").value = "";
+			PendingAction = "MakeDiscussionPost";	
+		}
+		if(ViewMode == "threads"){
+			ActionData = document.getElementById("submissionArea").value;
+			document.getElementById("submissionArea").value = "";
+			PendingAction = "MakeThread";
+		}
+		if(ViewMode == "thread"){
+			// TODO
+		}
+		if(ViewMode == "events"){
+			ActionData = document.getElementById("submissionArea").value;
+			document.getElementById("submissionArea").value = "";
+			ActionData += "," + prompt("Describe The Event Taking Place...");
+			ActionData += "," + prompt("When will the event happen?");
+			ActionData += "," + prompt("Where will the event occur?");
+			PendingAction = "MakeEvent";
+		}
 	}
 }
 
@@ -178,3 +275,29 @@ function loadGroupTrigger(e){
 	ActionData = ActionData.substring(2,ActionData.length); 
 	PendingAction = "LoadGroup";
 }
+
+function openDiscussionTrigger(){
+	ViewMode = "discussion";
+	LastModified = 0;
+}
+function threadsTrigger(){
+	ViewMode = "threads";
+	LastModified = 0;
+}
+function eventsTrigger(){
+	ViewMode = "events";
+	LastModified = 0;
+}
+function infoTrigger(){
+	ViewMode = "about";
+	LastModified = 0;
+}
+
+function openThreadTrigger(e){
+	alert(e.target.parentNode.id);
+	//ViewMode = "thread";
+	//ID_OF_DISCUSSION = ;
+	//LastModified = 0;
+}
+
+function escape(str) { return str; }
