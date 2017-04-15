ca50aee7275e2edbf55c54ff6c6285ad8369b851
IIMasterXII <erich.showalter@gmail.com>
2017-04-15 10:53:55 -0700

    finished index and login

---
+++ OAUTH_Handlers.go
@@ -0,0 +1,50 @@
+// Contains the handlers for our OAuth interactions.
+package main
+
+import (
+	"net/http"
+
+	"github.com/Esseh/goauth"
+	dropbox "github.com/Esseh/goauth-dropbox"
+	github "github.com/Esseh/goauth-github"
+	"github.com/Esseh/notorious-dev/AUTH"
+	"github.com/Esseh/notorious-dev/PATHS"
+	"github.com/julienschmidt/httprouter"
+)
+
+func init() {
+	goauth.GlobalSettings.ClientType = "appengine"
+	github.Config.ClientID = "e0297346f88565c9f443"
+	github.Config.SecretID = "7dd96d4a262a004aeffefe4b0af1a38e03b38d14"
+	github.Config.Redirect = "http://localhost:8080/login/github/oauth/recieve"
+	dropbox.Config.ClientID = "ddhu8e7nswl56yt"
+	dropbox.Config.SecretID = "387kru0n9nb0qkk"
+	dropbox.Config.Redirect = "http://localhost:8080/login/dropbox/oauth/recieve"
+}
+
+func INIT_OAUTH_Handlers(r *httprouter.Router) {
+	r.GET(PATHS.AUTH_OAUTH_GITHUB_Send, AUTH_OAUTH_GITHUB_Send)
+	r.GET(PATHS.AUTH_OAUTH_GITHUB_Recieve, AUTH_OAUTH_GITHUB_Recieve)
+	r.GET(PATHS.AUTH_OAUTH_DROPBOX_Send, AUTH_OAUTH_DROPBOX_Send)
+	r.GET(PATHS.AUTH_OAUTH_DROPBOX_Recieve, AUTH_OAUTH_DROPBOX_Recieve)
+}
+
+func AUTH_OAUTH_GITHUB_Send(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	github.Send(res, req)
+}
+
+func AUTH_OAUTH_GITHUB_Recieve(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	token := github.Recieve(res, req)
+	emailObj, _ := token.Email(req)
+	accountObj, _ := token.AccountInfo(req)
+	AUTH.OAuthLogin(req, res, emailObj.Email, accountObj.Login, "", token.State)
+}
+
+func AUTH_OAUTH_DROPBOX_Send(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	dropbox.Send(res, req)
+}
+func AUTH_OAUTH_DROPBOX_Recieve(res http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	token := dropbox.Recieve(res, req)
+	accountInfo, _ := token.AccountInfo(req)
+	AUTH.OAuthLogin(req, res, accountInfo.Email, accountInfo.NameDetails.GivenName, accountInfo.NameDetails.Surname, token.State)
+}
--- main.go
+++ main.go
@@ -1,21 +1,33 @@
 package main
+
 import (
-	"fmt"
-	"net/http"
 	"html/template"
+	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 )
+
 var TPL *template.Template
+
 func init() {
 	funcMap := template.FuncMap{}
 	TPL = template.New("").Funcs(funcMap)
 	TPL = template.Must(TPL.ParseGlob("templates/*"))
 }
 
+// Serves the index page.
+func index(res http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	TPL.ExecuteTemplate(res, "index", nil)
+}
+
+// Serves the index page.
+func login(res http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	TPL.ExecuteTemplate(res, "login", nil)
+}
+
 func init() {
 	router := httprouter.New()
-	router.GET("/", func(res http.ResponseWriter, req *http.Request, p httprouter.Params){
-		fmt.Fprint(res,"HELLO WORLD")
-	})
+	router.GET("/", index)
+	router.GET("/login", login)
 	http.Handle("/", router)
 }
